5156e3435d5dc0d1b389ccfbd959654992ab532b
luciayin9944 <luxiayin94@gmail.com>
2025-04-25 12:24:19 -0700

    update consecutive-substrings.js

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,6 +1,18 @@
 function consecutiveSubstrings(string) {
   // type your code here
+  //slidow window: use two pointers: start&end
+  const ans = [];
+  const n = string.length;
+
+  for (let start=0; start<n; start++) {
+    for (let end=start+1; end<=n; end++){
+      ans.push(string.slice(start, end));
+    }
+  }
+  return ans;
 }
+console.log(consecutiveSubstrings('efgh'));
+
 
 if (require.main === module) {
   // add your own tests in here
